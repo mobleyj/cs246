e5c3b12c66439f5ba9cbce60b0948cac2b16cac5
Jordan <mob08001@byui.edu>
2016-10-05 20:16:48 -0600

    This is step one Version Ctrl

---
+++ HelloWorld.java
@@ -0,0 +1,14 @@
+import java.io.FileReader;
+import java.io.FileNotFoundException;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class HelloWorld {
+   public static void main(String[] args){
+      
+      System.out.println("Hello World");
+      
+   }
+   
+}
